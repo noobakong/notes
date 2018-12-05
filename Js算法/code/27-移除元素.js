dc39d56497692b4a2bccb980c712f8dc93f10f8a
noobakong <2492161235@qq.com>
2018-12-05 11:18:55 +0800

    leedcode-27

--- Js算法/code/27-移除元素.js
+++ Js算法/code/27-移除元素.js
@@ -3,12 +3,12 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    for (var i = 0; i < nums.length; i++) {
-        if (nums[i] == val) {
-            nums.splice(i,1)
-            i--
-        }
+var removeElement = function (nums, val) {
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] == val) {
+      nums.splice(i, 1)
+      i--
     }
-    return nums.length
+  }
+  return nums.length
 };
