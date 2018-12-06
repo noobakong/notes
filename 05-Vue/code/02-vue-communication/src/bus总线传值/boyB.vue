cf8c35a594c802b52a28572700217f7c58b58d10
noobakong <2492161235@qq.com>
2018-12-06 21:55:39 +0800

    vue传值相关

---
+++ 05-Vue/code/02-vue-communication/src/bus总线传值/boyB.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="on">
+      <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+import Bus from './bus.js'
+export default {
+  data() {
+    return {
+      msg:  ''
+    }
+  },
+  mounted() {
+      let self = this
+      Bus.$on('msg', (e) => {
+      self.msg = e
+      window.console.log(`传来的数据是：${e}`)
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 05-Vue/code/02-vue-communication/src/components/Son.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{name}}</h2>
+    <h3>{{myage}}</h3>
+    <button @click='$emit("addAge")'>快快长大</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      name: 'noobakong',
+    }
+  },
+  props: {
+    msg: String,
+    myage: Number
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
