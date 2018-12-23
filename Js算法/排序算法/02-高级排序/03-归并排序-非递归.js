8e671dc98e8632ad0f296a237c6dcc7fdbe1bf73
noobakong <2492161235@qq.com>
2018-12-24 01:40:30 +0800

    头疼

--- Js算法/排序算法/02-高级排序/03-归并排序-非递归.js
+++ Js算法/排序算法/02-高级排序/03-归并排序-非递归.js
@@ -15,65 +15,7 @@ function insertionSort(arr, l, r) {
   // return arr
 }
 
-// 自底向上的归并排序
-// function mergeSortBottomToUp (arr) {
-//   var len = arr.length
-//   // 第一轮循环控制有几次向上的过程 1 - 2 - 4 - 8 。。。 这样的过程
-//   for (let size = 1; size <= len; size+=size) {
-//     // 对arr[i...i+size-1],和 arr[i+size....i+size*2-1] 进行归并
-//     // for (let i = 0; i< len; i += size*2) {
-//     // i+size也至少是小于n的 保证 i+size-1 是不会越界的
-//     for (let i = 0; i + size < len; i += size * 2) {
-//       // __merge(arr, i, i+size-1, i+size*2 - 1)
-//       // i+size*2 -1 可能会比n大 所以
-//       __merge(arr, i, i+size-1, Math.min((i+size*2-1),len-1))
-//     }
-//   }
-// }
-
-
-// 自然归并排序
-// 我们知道归并排序是将多个有序的数组合并成一个数组
-// 一个无序的数组（除了完全逆序的数组 ），总有一部分数组是有序的
-// 我们可以获取有序数组的标记，从而将多个有序数组合并成一个数组,这是自然归并排序的关键
-function __getIndex (arr) {
-  let flag = []
-  // 最开始下标为0
-  let next = 0
-  flag[next] = 0
-  next++
-  for (let i=0;i<arr.length;i++) {
-    // 找到数组开始无序的地方
-    if (arr[i] > arr[i+1]) {
-      flag[next++] = i
-    }
-  }
-
-  console.log(flag)
-  console.log(next)
-  return next
-}
-
-function naturallyMergeSort (arr) {
-  let flag = __getIndex(arr)
-  let num = flag[flag.length-1]
-  // 去flag最后一项为num，大于等于2 说明数组不是完全有序的
-  while (num>=2) {
-    // 对相邻数组进行合并
-    for (let i = 0;i <= num;i+=2) {
-      __merge(arr,flag[i],flag[i+1],flag[i+2])
-    }
-    flag = __getIndex(arr)
-    num = flag[flag.length - 1]
-  }
-  return arr
-}
-
-__getIndex([0, 3, 4, 1, 7, 5, 8, 2])
-
-
-
-
+// 两组的归并操作
 function __merge(arr, l, mid, r) {
   let aux = new Array(r - l + 1)
   let i = 0
@@ -95,6 +37,64 @@ function __merge(arr, l, mid, r) {
   }
 }
 
-// let arr = util.randomArray(50000, 0, 50000)
+// 自底向上的归并排序
+function mergeSortBottomToUp (arr) {
+  var len = arr.length
+  // 第一轮循环控制有几次向上的过程 1 - 2 - 4 - 8 。。。 这样的过程
+  for (let size = 1; size <= len; size+=size) {
+    // 对arr[i...i+size-1],和 arr[i+size....i+size*2-1] 进行归并
+    // for (let i = 0; i< len; i += size*2) {
+    // i+size也至少是小于n的 保证 i+size-1 是不会越界的
+    for (let i = 0; i + size < len; i += size * 2) {
+      // __merge(arr, i, i+size-1, i+size*2 - 1)
+      // i+size*2 -1 可能会比n大 所以
+      __merge(arr, i, i+size-1, Math.min((i+size*2-1),len-1))
+    }
+  }
+}
 
-// util.testSort('归并BU', mergeSortBottomToUp, arr)
+
+// 自然归并排序
+// 我们知道归并排序是将多个有序的数组合并成一个数组
+// 一个无序的数组（除了完全逆序的数组 ），总有一部分数组是有序的
+// 我们可以获取有序数组的标记，从而将多个有序数组合并成一个数组,这是自然归并排序的关键
+// let arr = [1, 5, 2, 3, 6, 0, 7, 4, 8]
+//         0, 1, 2, 3, 4, 5, 6, 7, 8
+//    arr[0-1] arr[2-4] arr[5-6] arr[7-8]
+
+// 自然分组
+function fenzu (arr,getPosArr) {
+  let j = 0
+  getPosArr[j++] = 0  // 第一个分组一定是从下标0开始的
+  for(i=0 ;i < arr.length -1;i++) {
+    if (arr[i] > arr[i+1]) {
+      getPosArr[j++] = i
+    }
+  }
+  // let arr = [1, 5, 2, 3, 6, 0, 7, 4, 8]
+  // numOfNatureGroupings为自然分组的个数, 此次值为4，
+  // 即分为四组，{1, 5}, {2, 3, 6}, {0, 7}, {4, 8}
+  return j
+}
+
+function naturallyMergeSort (arr) {
+  let getPosArr = []
+  let groupsNum = fenzu(arr,getPosArr)
+  while(groupsNum>2) {
+    for(i=0;i<groupsNum-2;i+=2) {
+      __merge(arr,getPosArr[i],getPosArr[i+1],getPosArr[i+2])
+    }
+    groupsNum = fenzu(arr,getPosArr)
+  }
+  if(groupsNum == 2) {
+    __merge(arr,getPosArr[0],getPosArr[1],arr.length-1)
+  }
+  return arr
+}
+
+
+
+let arr = util.randomArray(500000, 0, 50000)
+let arr1 = arr.slice()
+util.testSort('归并BU', mergeSortBottomToUp, arr)
+// util.testSort('自然归并', naturallyMergeSort, arr1)
