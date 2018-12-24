6b6f0a2dc0d018de7c2c2d52fa92d79f3d18dbe4
noobakong <2492161235@qq.com>
2018-12-24 15:49:18 +0800

    StrandSort

--- Js算法/排序算法/02-高级排序/03-归并排序-非递归.js
+++ Js算法/排序算法/02-高级排序/03-归并排序-非递归.js
@@ -1,5 +1,18 @@
 const util = require('../util/util')
 
+function testSort (sortName, sortFn, arr) {
+  console.time(sortName);
+  sortFn(arr);
+  console.timeEnd(sortName);
+}
+function isSorted (arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1])
+      return false
+  }
+  return true
+}
+
 function insertionSort(arr, l, r) {
   // 默认第一个位置与自己相比已经有序，i从1开始而不是0
   for (let z = l; z <= r; z++) {
@@ -66,11 +79,14 @@ function mergeSortBottomToUp (arr) {
 function fenzu (arr,getPosArr) {
   let j = 0
   getPosArr[j++] = 0  // 第一个分组一定是从下标0开始的
-  for(i=0 ;i < arr.length -1;i++) {
+  // 循环到后面的时候，k前的元素已经排序完成 就不要再扫描k前的了
+  k = getPosArr[2] ? getPosArr[2] - 1 : 0
+  for(i= k ;i < arr.length -1;i++) {
     if (arr[i] > arr[i+1]) {
-      getPosArr[j++] = i
+      getPosArr[j++] = i 
     }
   }
+  getPosArr[j++] = arr.length-1
   // let arr = [1, 5, 2, 3, 6, 0, 7, 4, 8]
   // numOfNatureGroupings为自然分组的个数, 此次值为4，
   // 即分为四组，{1, 5}, {2, 3, 6}, {0, 7}, {4, 8}
@@ -86,15 +102,79 @@ function naturallyMergeSort (arr) {
     }
     groupsNum = fenzu(arr,getPosArr)
   }
-  if(groupsNum == 2) {
-    __merge(arr,getPosArr[0],getPosArr[1],arr.length-1)
-  }
   return arr
 }
 
+// let arr = [1, 5, 2, 3, 6, 0, 7, 4, 8]
 
 
-let arr = util.randomArray(500000, 0, 50000)
+// let arr = util.randomArray(500000, 0, 50000)
+// // let arr1 = arr.slice()
+// // util.testSort('归并BU', mergeSortBottomToUp, arr)
+// util.testSort('自然归并', naturallyMergeSort, arr)
+
+function StrandSort (arr) {
+  let aux = [[arr[0]]]
+  let times = 0
+  let arr2 = []
+  let flag = 0
+  while(flag === 0) {
+    let temp = arr[0]
+    aux[times] = [arr[0]]
+    // flag = 1
+    for(var i=1;i<arr.length;i++) {
+      if(arr[i] >= temp) {
+        // flag = arr[i] === temp ? 1 : 0
+        temp = arr[i]
+        aux[times].push(arr[i])
+      } else {
+        arr2.push(arr[i])
+      }
+    }
+    if (arr2.length) {
+      arr = arr2.slice()
+    } else {
+      flag = 1
+    }
+    arr2 = []
+    if (times === 1) {
+      aux[0] = __merge2(aux[0],aux[1])
+      times--
+    }
+    times++
+    
+  }
+  return aux[0]
+}
+
+function __merge2(arr1,arr2) {
+  let aux = new Array(arr1.length + arr2.length)
+  let i = 0 
+  let p1 = 0
+  let p2 = 0
+  // 谁小往临时数组里就添谁的过程
+  while (p1 <= arr1.length-1 && p2 <= arr2.length -1) {
+    aux[i++] = arr1[p1] < arr2[p2] ? arr1[p1++] : arr2[p2++]
+  }
+  // 临界过程
+  while (p1 <= arr1.length -1) {
+    aux[i++] = arr1[p1++]
+  }
+  while (p2 <= arr2.length -1) {
+    aux[i++] = arr2[p2++]
+  }
+  return aux
+}
+
+// console.log(StrandSort([ 0, 1, 2, 5, 4, 5 ]))
+let arr = util.randomArray(10000, 0, 5)
 let arr1 = arr.slice()
-util.testSort('归并BU', mergeSortBottomToUp, arr)
-// util.testSort('自然归并', naturallyMergeSort, arr1)
+// console.log(StrandSort([1, 1, 0, 0, 4]))
+// util.testSort('归并BU', mergeSortBottomToUp, arr)
+// util.testSort('自然归并', naturallyMergeSort, arr)
+// util.testSort('strandSort',StrandSort , arr)
+let res = StrandSort(arr)
+console.log(res)
+console.log(isSorted(res))
+
+// testSort('strandsort',StrandSort,arr)
